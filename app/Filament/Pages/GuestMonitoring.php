9cb6c95d725245f017878aaa6fe2675deca22d74
Rustom Pedales <pedales.rustom@gmail.com>
2024-07-22 18:37:27 +0800

    [Update]: Add Guest mode

---
+++ app/Filament/Pages/GuestMonitoring.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Pages;
+
+use Filament\Pages\Page;
+
+class GuestMonitoring extends Page
+{
+    protected static ?string $navigationIcon = 'heroicon-o-document-text';
+
+    protected static string $view = 'filament.pages.guest-monitoring';
+}
